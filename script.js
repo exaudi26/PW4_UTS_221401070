dde3d49d618a6ca6ce76ec6ab5249c991b39b22b
Hotbaen_070 <messijr.10hes@gmail.com>
2023-11-17 15:01:57 +0700

    PW4

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function toggleBackgroundColor() {
-    // Get the body element
-    var body = document.body;
-
-    // Toggle between two CSS classes
-    if (body.classList.contains('background-color-1')) {
-        body.classList.remove('background-color-1');
-        body.classList.add('background-color-2');
-    } else {
-        body.classList.remove('background-color-2');
-        body.classList.add('background-color-1');
-    }
-}
-
-  var lastScrollTop = 0;
+var lastScrollTop = 0;
 
   window.addEventListener("scroll", function () {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
